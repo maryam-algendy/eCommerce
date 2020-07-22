7261cb7e47c10084e8649c85bdf85e0a721526a6
maryam-algendy <maryamalgendy@gmail.com>
2020-07-22 12:51:32 +0200

    add carousel

---
+++ src/Layout/components/Carousel.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {Carousel,Button} from "react-bootstrap";
+
+
+const items=
+[
+    {
+        paragraph:"paragraph1",
+        header:"header1",
+        btn:"btn1",
+        link:"carousel-1.jpg"
+    },
+    {
+        paragraph:"paragraph2",
+        header:"header2",
+        btn:"btn3",
+        link:"carousel-2.jpg"
+    },
+    {
+        paragraph:"paragraph3",
+        header:"header3",
+        btn:"btn3",
+        link:"carousel-3.jpg"
+    }
+
+
+]
+
+class carousel extends React.Component{
+    render(){
+        return(
+            <div className="Carousel">
+                 <Carousel>
+{
+    items.map((item,id)=>{
+        return(
+            <Carousel.Item key={id} style={{background: `url(${item.link}) center` , backgroundSize:"cover" ,height:"570px"}}>
+            <Carousel.Caption>
+                <p style={{animation:`${item.paragraph} 2s forwards`}}>women collection 2018</p>
+                <h2 style={{animation:`${item.header} 2s forwards` ,animationDelay:"2s"}}>new arrivals</h2>
+                <Button style={{animation:`${item.btn} forwards 2s`, animationDelay:"3s"}}>shop now</Button>
+            </Carousel.Caption>
+        </Carousel.Item>
+        )
+
+    })
+}
+            </Carousel>
+
+            </div>
+           
+            )
+        }
+    }
+
+export default carousel;
