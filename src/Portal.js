99cebfcd388e86873b27bd695730013505ae0d4f
maryam-algendy <maryamalgendy@gmail.com>
2020-07-26 08:24:25 +0200

    add feature products

--- src/Portal.js
+++ src/Portal.js
@@ -2,7 +2,8 @@ import React from 'react';
 import Navbar from "./Layout/components/Navbar";
 import Carousel from "./Layout/components/Carousel";
 import Categories from "./Layout/components/Categories";
-import Counter from "./Layout/components/Counter"
+import Counter from "./Layout/components/Counter";
+import Features from "./Layout/components/Features"
 
 function Portal() {
   return (
@@ -11,6 +12,7 @@ function Portal() {
       <Carousel/>
       <Categories/>
       <Counter/>
+      <Features/>
     </div>
   );
 }
