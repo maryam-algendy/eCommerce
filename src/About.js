c8149f1e9a42f0885c0164cf357b6c0dd819d4bd
maryam-algendy <maryamalgendy@gmail.com>
2020-08-14 06:20:46 +0200

    add about page

--- src/About.js
+++ src/About.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Layout/components/Header';
+import OurStory from './Layout/components/OurStory'
 
 function About() {
     return (
       <div>
         <Header img={"contact.jpg"} name={"about"}/>
+        <OurStory/>
       </div>
     );
   }
