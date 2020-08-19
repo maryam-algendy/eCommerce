b21d2b6b95d7312937de832012b7bb183b0255ca
maryam-algendy <maryamalgendy@gmail.com>
2020-08-19 18:47:48 +0200

    add sale page

--- src/Sale.js
+++ src/Sale.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Layout/components/Header';
+import SaleContent from './Layout/components/SaleContent';
 
 function Sale() {
     return (
       <div>
         <Header img={"sale.jpg"} name={"women"} desc={"New Arrivals Women Collection 2018"}/>
+        <SaleContent/>
       </div>
     );
   }
